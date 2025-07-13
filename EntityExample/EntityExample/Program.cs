9053f7c3511d297722aa5c6160116a3fc8f44b85
novakvova <novakvova@gmail.com>
2025-07-13 14:50:52 +0300

    Is good insert user

--- EntityExample/EntityExample/Program.cs
+++ EntityExample/EntityExample/Program.cs
@@ -1,4 +1,20 @@
-﻿Console.InputEncoding = System.Text.Encoding.UTF8;
+﻿using EntityExample.Data;
+using EntityExample.Data.Entities;
+
+Console.InputEncoding = System.Text.Encoding.UTF8;
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 Console.WriteLine("Хороша погода");
+
+var user = new UserEntity
+{
+    FistName = "Іван",
+    LastName = "Брусок",
+    Email = "ivan@gmail.com"
+};
+
+using var context = new AppBeaverContext();
+context.Users.Add(user); // Додаємо нового користувача до контексту
+context.SaveChanges(); // Зберігаємо зміни в базі даних
+
+Console.WriteLine($"User id = {user.Id}");
